84eae73de966a73d10508d44fc8e25376d91a0f4
peter.chang <peter.chang@aplus-digital.com>
2021-07-28 18:28:18 +0800

    add size input to ring cursor

--- src/app/shared/ring-cursor/ring-cursor.component.ts
+++ src/app/shared/ring-cursor/ring-cursor.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ring-cursor',
   templateUrl: './ring-cursor.component.html',
-  styleUrls: ['./ring-cursor.component.scss']
+  styleUrls: ['./ring-cursor.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RingCursorComponent implements OnInit {
+  @Input() ringSize = 50;
 
-  constructor() { }
+  constructor(private cd: ChangeDetectorRef) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  updateSize(size: number) {
+    this.ringSize = size;
+    this.cd.markForCheck();
   }
-
 }
