5fa171ea7dddad74f9732cb4c97cf6bb09d57125
peter.chang <peter.chang@aplus-digital.com>
2021-07-29 18:36:57 +0800

    add simple parallax scrolling

--- src/app/pages/parallax-scroll/parallax-scroll.component.ts
+++ src/app/pages/parallax-scroll/parallax-scroll.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-parallax-scroll',
   templateUrl: './parallax-scroll.component.html',
-  styleUrls: ['./parallax-scroll.component.scss']
+  styleUrls: ['./parallax-scroll.component.scss'],
 })
-export class ParallaxScrollComponent implements OnInit {
+export class ParallaxScrollComponent {
+  mainTl?: GSAPTimeline;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.mainTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.bg-block',
+        pin: true,
+        start: 'top top+=200px',
+        end: '+=500',
+        scrub: 1,
+      },
+    });
+
+    this.mainTl.from('.box', { y: 300, ease: 'none' }).to('.box', { y: -300, ease: 'none' });
   }
-
 }
